fd1c0b1677fee0593c4ec870d2d7b78e2133d3fc
ANURAG CHOUDHARY <anuragchoudhary126@gmail.com>
2026-03-28 14:07:00 +0530

    second commit

--- src/Api/auth.api.js
+++ src/Api/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // apna backend url
+  baseURL: "https://roommate-backend.vercel.app/api", // apna backend url
   withCredentials: true,
 });
 
