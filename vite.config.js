b766870e24688a1de34cc51a85e9a9d7f7bb9d0b
ANURAG CHOUDHARY <anuragchoudhary126@gmail.com>
2026-03-28 15:28:51 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://roommate-backend.vercel.app/",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
