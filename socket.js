36dca82b49645f59db08ca839daebd196072c62d
ANURAG CHOUDHARY <anuragchoudhary126@gmail.com>
2026-03-31 18:23:23 +0530

    versel commit

--- socket.js
+++ socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://roommate-backend-bh0y1q6g1-anurag-choudharys-projects-c0129f03.vercel.app", {
   withCredentials: true,
 });
